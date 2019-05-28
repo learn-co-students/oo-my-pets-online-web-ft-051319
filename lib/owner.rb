2287b830bd4b7f38151f6aadfeb34fd35cc2f78d
Denali Balser <denali.balser167@gmail.com>
2019-05-28 15:05:10 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,11 +4,12 @@ class Owner
   attr_accessor :name, :pets, :fish, :dog, :cat
 
   @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
+  #@@pets = {:fishes => [], :dogs => [], :cats => []}
 
   def initialize(species)
     @species = species
     @name = name
+    @pets = {:fishes => [], :dogs => [], :cats => []}
     @@all << self
   end
 
@@ -29,44 +30,44 @@ class Owner
   end
 
   def pets
-    @@pets
+    @pets
   end
 
   def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
+    @pets[:cats] << Cat.new(cat)
   end
 
   def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+    @pets[:dogs] << Dog.new(dog)
   end
 
   def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
+    @pets[:fishes] << Fish.new(fish)
   end
 
   def walk_dogs
-    @@pets[:dogs].each do |dog|
+    @pets[:dogs].each do |dog|
       dog.mood = 'happy'
     end
   end
 
   def play_with_cats
-    @@pets[:cats].each do |cat|
+    @pets[:cats].each do |cat|
       cat.mood = 'happy'
     end
   end
 
   def feed_fish
-    @@pets[:fishes].each do |fish|
+    @pets[:fishes].each do |fish|
       fish.mood = 'happy'
     end
   end
 
   def sell_pets
-    @@pets.collect do |species, instances|
+    @pets.each do |species, instances|
       instances.each do |pet|
-        pet.mood = "nervous"
-    
+        pet.mood = 'nervous'
+        #binding.pry
       end
       instances.clear
     end
